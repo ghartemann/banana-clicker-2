9a7a7463b4506183fe758aa3cefdb2f0a09cb1c9
ghartemann <g.hartemann@gmail.com>
2023-06-15 14:20:27 +0200

    save

--- assets/js/pages/home/index.vue
+++ assets/js/pages/home/index.vue
@@ -2,39 +2,43 @@
     <div class="tw-container">
         <h1 class="tw-text-5xl tw-text-center tw-my-10">Banana Clicker 2</h1>
 
-        <div class="tw-grid tw-grid-cols-3">
-            <div class="tw-col-span-1 tw-flex tw-flex-col tw-justify-center tw-text-center">
+        <div class="tw-grid tw-grid-cols-3 tw-gap-10">
+            <div class="tw-col-span-1 tw-flex tw-flex-col tw-justify-center">
                 <div>BPS</div>
 
-                <div>
-                    <v-btn v-for="bpsModule in bpsModules"
-                           @click="buyBpsModule(bpsModule)"
-                           :disabled="bananas < bpsModule.price"
-                           color="secondary"
-                    >
-                        {{ bpsModule.name}} (+{{ bpsModule.bps }} BPS, {{ bpsModule.price }} <v-icon>mdi-fruit-cherries</v-icon>) ({{ bpsModule.numberBought }})
-                    </v-btn>
+                <div class="tw-flex tw-flex-col tw-gap-4">
+                    <module-button v-for="bpsModule in bpsModules"
+                                   :bananas="bananas"
+                                   :module="bpsModule"
+                                   type="bps"
+                                   @buy="(module) => buyBpsModule(module)">
+                    </module-button>
                 </div>
             </div>
 
-            <div class="tw-col-span-1 tw-flex tw-flex-col tw-justify-center tw-text-center">
-                <div>Bananes : {{ Math.round(bananas) }}</div>
-                <div>BPS : {{ bps }}</div>
-                <div>BPC : {{ bpc }}</div>
-                <v-btn @click="getBanana" color="primary">Obtenir des bananes</v-btn>
+            <div class="tw-col-span-1 tw-flex tw-flex-col tw-items-center tw-text-center animate-bounce delay-150 duration-300">
+                <button class="tw-bg-green-dark tw-rounded-full tw-w-fit tw-p-8 tw-shadow tw-m-5" @click="getBanana">
+                    <img src="../../../images/banane.png" alt="Banana" class="tw-w-36 tw-h-36 ">
+                </button>
+
+                <div class="tw-font-bold tw-text-6xl">{{ Math.round(bananas) }}</div>
+
+                <div>
+                    <div>BPS : {{ bps }}</div>
+                    <div>BPC : {{ bpc }}</div>
+                </div>
             </div>
 
             <div class="tw-col-span-1 tw-flex tw-flex-col tw-justify-center tw-text-center">
                 <div>BPC</div>
 
-                <div>
-                    <v-btn v-for="bpcModule in bpcModules"
-                           @click="buyBpcModule(bpcModule)"
-                           :disabled="bananas < bpcModule.price"
-                           color="secondary"
-                    >
-                        {{ bpcModule.name}} (+{{ bpcModule.bpc }} BPC, {{ bpcModule.price }} <v-icon>mdi-fruit-cherries</v-icon>) ({{ bpcModule.numberBought }})
-                    </v-btn>
+                <div class="tw-flex tw-flex-col tw-gap-4">
+                    <module-button v-for="bpcModule in bpcModules"
+                                   :bananas="bananas"
+                                   :module="bpcModule"
+                                   type="bpc"
+                                   @buy="(module) => buyBpcModule(module)">
+                    </module-button>
                 </div>
             </div>
         </div>
@@ -43,9 +47,11 @@
 
 <script>
 import {defineComponent} from 'vue'
+import ModuleButton from "./components/module-button.vue";
 
 export default defineComponent({
     name: "Home",
+    components: {ModuleButton},
     data() {
         return {
             bananas: 0,
@@ -54,19 +60,28 @@ export default defineComponent({
             bpsModules: [
                 {
                     name: 'Auto Clicker',
+                    article: 'un',
+                    description: 'Un curseur qui clique tout seul comme un grand. Si vous êtes un tant soit peu impressionnable ça vous fera peut-être quelque chose.',
+                    picture: 'autoclicker',
                     price: 10,
                     bps: 1,
                     numberBought: 0
                 },
                 {
                     name: 'Bananier',
-                    price: 100,
+                    article: 'un',
+                    description: "Un bananier. Qui produit des bananes. Écoutez c'est un arbre, il n'y a pas non plus un million de choses à dire dessus. Si vous êtes plus impressionné.e par ça que par l'auto clicker je pense qu'il faudrait songer à arrêter de jouer dès maintenant.",
+                    picture: 'bananier',
+                    price: 500,
                     bps: 5,
                     numberBought: 0
                 },
                 {
-                    name: 'Ferme à bananes',
-                    price: 500,
+                    name: 'Macaque',
+                    article: 'un',
+                    description: 'Un macaque qui ramasse des bananes. On le paie une misère et il ne semble pas s\'en plaindre mais niveau efficacité on repassera.',
+                    picture: 'macaque',
+                    price: 1000,
                     bps: 10,
                     numberBought: 0
                 }
@@ -74,7 +89,10 @@ export default defineComponent({
             bpcModules: [
                 {
                     name: 'Meilleur curseur',
-                    price: 500,
+                    article: 'un',
+                    description: '',
+                    picture: '',
+                    price: 200,
                     bpc: 1,
                     numberBought: 0
                 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: [
         "./assets/**/*.js",
@@ -7,7 +8,25 @@ module.exports = {
     ],
     prefix: "tw-",
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'white': '#ffffff',
+                'purple': '#3f3cbb',
+                'green-darker': '#0f5b01',
+                'green-dark': '#27ae60',
+                'green-light': '#2ecc71',
+                'green-dark-transparent': '#27ae5f6d',
+                'yellow-darker': '#df7a0e',
+                'yellow-dark-transparent': '#f39d1258',
+                'yellow-dark': '#f39c12',
+                'yellow-light': '#f1c40f',
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        require('tailwindcss-animated'),
+        require('tailwindcss-animate'),
+    ],
 }
